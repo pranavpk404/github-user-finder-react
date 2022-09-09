af1e0dc1a61cc94758eb690408d5a99c88860d8d
Pranav <pranavpk404@gmail.com>
2022-09-09 18:50:03 +0530

    Improved error handling and changed Ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./components/SearchBar";
 import RepoDetails from "./components/RepoDetails";
 import UserDetails from "./components/UserDetails";
 
-function App() {
+const App = () => {
   const [searched, setSearched] = useState(false);
   const [userName, setUserName] = useState("");
   const [userDetails, setUserDetails] = useState({
@@ -21,53 +21,62 @@ function App() {
   useEffect(() => {
     const getUserDetails = async () => {
       let url = `https://api.github.com/users/${userName}`;
-      let data = await fetch(url);
-
-      let user = await data.json();
-      if (user.message === "Not Found") {
-        alert("User not found");
-        setUserDetails({
-          avatar_url: "",
-          blog: "",
-          bio: "",
-          html_url: "",
-          location: "",
-          login: "",
-          name: "",
-          twitter_username: "",
-        });
-      } else {
-        setUserDetails(user);
+      try {
+        let data = await fetch(url);
+        let user = await data.json();
+        if (user.message === "Not Found") {
+          alert("User not found");
+          setUserDetails({
+            avatar_url: "",
+            blog: "",
+            bio: "",
+            html_url: "",
+            location: "",
+            login: "",
+            name: "",
+            twitter_username: "",
+          });
+        } else {
+          setUserDetails(user);
+        }
+      } catch (error) {
+        alert("server error");
       }
     };
     const getUserRepo = async () => {
-      let url = `https://api.github.com/users/${userName}/repos`;
-      let data = await fetch(url);
+      try {
+        let url = `https://api.github.com/users/${userName}/repos`;
+        let data = await fetch(url);
 
-      let resp = await data.json();
-      if (resp.message === "Not Found") {
-        setUserRepo([]);
-      } else {
-        setUserRepo(resp);
+        let resp = await data.json();
+        if (resp.message === "Not Found") {
+          setUserRepo([]);
+        } else {
+          setUserRepo(resp);
+        }
+      } catch (error) {
+        console.log(error);
       }
     };
-
-    if (searched) {
+    if (searched ) {
       getUserDetails();
       getUserRepo();
-    } else {
-      setSearched(true);
-      setUserName("pranavpk404");
     }
   }, [userName]);
 
   return (
-    <main className="font-Poppins bg-gray-900 text-white">
+    <main className="font-Poppins first-letter:placeholder::bg-gray-900 text-white">
       <SearchBar setUserName={setUserName} setSearched={setSearched} />
-      <UserDetails userDetails={userDetails} />
-      <RepoDetails userRepo={userRepo} />
+      {searched ? (
+        <>
+          <UserDetails userDetails={userDetails} />
+          <RepoDetails userRepo={userRepo} />
+        </>
+      ) : (
+        <h1 className="text-center text-4xl my-52">Search for users</h1>
+      )}
     </main>
   );
-}
+};
 
 export default App;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,25 +8,29 @@ const SearchBar = ({ setUserName, setSearched }: SearchBarProps) => {
     setUserName(text);
   };
   return (
-    <div className="relative sm:ml-48 md:ml-72 p-5">
-      <input
-        type="text"
-        id="searchBar"
-        className="sm:w-96 w-80 h-10 bg-gray-800  rounded-full shadow-sm outline-none p-6 text-white hover:shadow-lg"
-        placeholder="Search Github"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.keyCode === 13) {
-            search();
-          }
-        }}
+    <div className="p-5 flex justify-between">
+      <img
+        src="https://img.icons8.com/plasticine/48/000000/github.png"
+        alt="icon"
       />
-      {text !== "" && (
+      <span>
+        <input
+          type="text"
+          id="searchBar"
+          className="sm:w-96 w-80 h-10 bg-gray-800  rounded-full shadow-sm outline-none p-6 text-white hover:shadow-lg"
+          placeholder="Search Github"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.keyCode === 13) {
+              search();
+            }
+          }}
+        />
         <button onClick={search} id="searchBtn" className="-m-11">
           🔎
         </button>
-      )}
+      </span>
     </div>
   );
 };
